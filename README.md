167c948ca557ee26ed42181baa32a4a893cc19f6
eyh1 <eryhuang@ucsc.edu>
2025-02-10 17:24:39 -0800

    moved stuff

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ pip install Flask
 pip install Flask-Cors
 export FLASK_APP=main.py
 python3 -m flask run --host=0.0.0.0
+
+How to connect to the AWS intance:
+chmod 400 "boba_app_eric.pem"
+ssh -i "boba_app_eric.pem" ubuntu@ec2-54-193-65-182.us-west-1.compute.amazonaws.com
--- flask_server/main.py
+++ flask_server/main.py
@@ -2,7 +2,7 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="../templates")
 
 # Allow React frontend to communicate with Flask backend
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
