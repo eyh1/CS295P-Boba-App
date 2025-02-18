026653a5042b2b6df6af7d25db396db9099eb2e5
Destin <destinwong@gmail.com>
2025-02-18 00:06:07 -0800

    Added stylings to login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,16 @@
+import "../styles/Login.css"
 function Login() {
-    return <div>
-        <h1>Login</h1>
-        <form>
-            <label><b>Username</b></label>
-            <input type="text" placeholder = "Enter Username" name = "username" required/>
-            <label><b>Password</b></label>
-            <input type="password" placeholder = "Enter Password" name = "password" required/>
-            <button>Login</button>
-        </form>
+    return <div class = "login-container">
+        <div class = "login-box">
+            <h1>Login</h1>
+            <form>
+                <label><b>Username</b></label>
+                <input type="text" placeholder = "Enter Username" name = "username" required/>
+                <label><b>Password</b></label>
+                <input type="password" placeholder = "Enter Password" name = "password" required/>
+                <button>Login</button>
+            </form>
+        </div>
     </div>
 }
 
