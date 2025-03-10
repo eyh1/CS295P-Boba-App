6fb1fad445c0e315693cbf35eb438701014d98f2
Destin <destinwong@gmail.com>
2025-03-09 17:20:16 -0700

    Made Profile prettier and added delete button

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,7 +2,15 @@ import { useState, useEffect } from "react";
 import api from "../api";
 import { ACCESS_TOKEN } from "../constants";
 import TopBar from "../components/TopBar";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 function Profile() {
     const [reviews, setReviews] = useState([]);
@@ -25,10 +33,21 @@ function Profile() {
         }
     };
 
-    // const formatDate = (dateString) => {
-    //     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-    //     return new Date(dateString).toLocaleDateString(undefined, options);
-    // };
+    const deleteReview = (id) => {
+        api
+            .delete(`/api/review/${id}/delete/`)
+            .then((res) => {
+                if (res.status === 204) alert("Review deleted!");
+                else alert("Failed to delete review.");
+                fetchUserReviews();
+            })
+            .catch((error) => alert(error));
+    };
+
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    };
     
     return <div>
     {/* <TopBar/> */}
@@ -36,16 +55,59 @@ function Profile() {
     <h1>Your Profile</h1>
     <h2>Your Reviews</h2>
     {reviews.length > 0 ? (
-        <ul>
+        <ul style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 0 }}>
             {reviews.map((review) => (
-                <li key={review.id}>
-                    <h3>Restaurant Name: {review.restaurant_name}</h3>
-                    <p>Content: {review.content}</p>
-                    <p>Rating: {review.review_category_ratings[0].rating}</p>
-                    <p>Price: {review.pricing}</p>
-                    <p>Sweetness: {review.sweetness}</p>
-                    <p>Posted on: {review.created_at}</p>
-                </li>
+                <Card variant="outlined" sx={{ maxWidth: 360 }}>
+                <Box sx={{ p: 2 }}>
+                  <Stack
+                    direction="row"
+                    sx={{ justifyContent: 'space-between', alignItems: 'center' }}
+                  >
+                    <Typography gutterBottom variant="h5" component="div">
+                    Restaurant Name: {review.restaurant_name}
+                    </Typography>
+                    <Typography gutterBottom variant="h6" component="div">
+                    ${review.pricing}
+                    </Typography>
+                  </Stack>
+                  <Typography variant="body1" sx={{ color: 'text.secondary' }}>
+                  {review.content}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  Posted on: {formatDate(review.created_at)}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                    Rating: {review.review_category_ratings[0].rating}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                    Category: {review.review_category_ratings[0].category}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                    Sweetness: {review.sweetness}
+                    </Typography>
+                </Box>
+                <Divider />
+                <Box sx={{ p: 2 }}>
+                  <IconButton variant="contained" className = "delete-button" onClick={() => deleteReview(review.id)}>
+                  <DeleteIcon fontSize="inherit" />
+                   </IconButton>
+                </Box>
+              </Card>
+            //     <Card variant="outlined" sx={{ maxWidth: 360 }}>
+            //     <li key={review.id}>
+            //     <Box sx={{ p: 2 }}>
+            //         <h3>Restaurant Name: {review.restaurant_name}</h3>
+            //         <p>Content: {review.content}</p>
+            //         <p>Rating: {review.review_category_ratings[0].rating}</p>
+            //         <p>Price: {review.pricing}</p>
+            //         <p>Sweetness: {review.sweetness}</p>
+            //         <p>Posted on: {formatDate(review.created_at)}</p>
+            //         <button className="delete-button" onClick={() => deleteReview(review.id)}>
+            //     Delete
+            // </button>
+            // </Box>
+            //     </li>
+            //     </Card>
             ))}
         </ul>
     ) : (
