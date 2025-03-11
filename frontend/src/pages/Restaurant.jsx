b65c944d637a080ad38db4dbac1d17024f63844e
eyh1 <eryhuang@ucsc.edu>
2025-03-11 13:38:15 -0700

    added tag for rest page

--- frontend/src/pages/Restaurant.jsx
+++ frontend/src/pages/Restaurant.jsx
@@ -178,7 +178,9 @@ function Restaurant() {
   const location = useLocation();
   const { name_from_home, pic_from_home, ratings_from_home, rest_id } = location.state || {};
   const [reviews, setReviews] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
   const [reviewJson, setReviewJson] = useState([]);
+  const [currentRest, setCurrentRest] = useState();
   
 
   const checkLoginStatus = () => {
@@ -190,8 +192,29 @@ function Restaurant() {
     checkLoginStatus();
     if (rest_id) {
     getRestaurantReviews(rest_id);
+    getRestaurants(rest_id);
   }
   }, [rest_id]);
+
+  const getRestaurants = (id) => {
+    api
+      .get("api/restaurants/")
+      .then((res) => res.data)
+      .then((data) => { 
+          setRestaurants(data); 
+          
+          if (data.length > 0 && data[0].restaurant_category_ratings) {
+            const fetchedRests = data.filter(item => item.id === id);
+            setCurrentRest((fetchedRests));
+            console.log("rest data is", fetchedRests); 
+
+        }})
+      .catch((error) => alert(error));
+    
+    
+    
+  };
+
 
   const getRestaurantReviews = (id) => {
     api
@@ -258,15 +281,6 @@ function Restaurant() {
     </Navbar>
   );
 }
-  function RatingCard({ entry_name, rating }) {
-    return (
-      <Card className="text-center shadow-sm border-0 rounded-pill bg-light px-3 py-2 mb-2">
-        <Card.Body className="p-1">
-          <strong>{entry_name}</strong> {rating} ⭐
-        </Card.Body>
-      </Card>
-    );
-  }
 
   function CategoryRatingCard({ category, rating }) {
   return (
@@ -301,18 +315,25 @@ function Restaurant() {
   );
 }
 
-  function EntryCard({ restaurant, pic_source, rating1, rating2, rating3, rest_id }) {
+  function EntryCard({ restaurant, pic_source, rating1, rating2, rating3, rest_id, restaurant_category_ratings}) {
+
+    // console.log("categs is ", restaurant_category_ratings);
+    
     return (
       <Card className="m-3 shadow-sm">
         <div className="card-grid-container-restaurant">
           <img src={pic_source} className="drink-cha" />
           <div className="card-inside-grid-container-restaurant">
             <h1 className="titleChaRestaurant">{restaurant}</h1>
-            <div className="ratings-container">
-              <RatingCard entry_name="Creme Brule" rating={rating1} />
-              <RatingCard entry_name="Matcha" rating={rating2} />
-              <RatingCard entry_name="Fruit Tea" rating={rating3} />
-            </div>
+            <div className="d-flex flex-wrap justify-content-center mt-2">
+          {restaurant_category_ratings.map((category_rating, index) => (
+            <CategoryRatingCard
+              key={index}
+              category={category_rating.category_name}
+              rating={category_rating.rating}
+            />
+          ))}
+        </div>
           </div>
         </div>
         <h1 className="titleChaRestaurant">Reviews:</h1>
@@ -348,8 +369,6 @@ function Restaurant() {
     { pic: pic_from_home, name: name_from_home, ratings: [ratings_from_home[0], ratings_from_home[1], ratings_from_home[2]] },
   ];
 
-
-
   return (
     <div className="grid-container-restaurant">
       {entries.map((entry, index) => (
@@ -361,6 +380,7 @@ function Restaurant() {
           rating2={entry.ratings[1]}
           rating3={entry.ratings[2]}
           rest_id={rest_id}
+          restaurant_category_ratings={currentRest ? (currentRest[0].restaurant_category_ratings) : ([{category_name: "Pilk Tea", rating: 3}])}
         />
       ))}
       
