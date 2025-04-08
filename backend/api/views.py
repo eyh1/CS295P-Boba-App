563e00941b0474d6b1047331a158239c59bf9732
Wang <michael.wang1@assaabloy.com>
2025-04-07 20:46:08 -0700

    added filter system to list restaurant view

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer, RestaurantSerializer, ReviewSerializer, RestaurantCategoryRatingSerializer, CategorySerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .models import Restaurant, Review, Category, ReviewCategoryRating
+from .models import Restaurant, Review, Category, ReviewCategoryRating, RestaurantCategoryRating
 from django.db.models import Avg
 from rest_framework.response import Response
 
@@ -15,7 +15,24 @@ class CreateUserView(generics.CreateAPIView):
 class ListRestaurantView(generics.ListAPIView):
     serializer_class = RestaurantSerializer
     permission_classes = [AllowAny]
-    queryset = Restaurant.objects.all()
+    
+    def get_queryset(self):
+        categories = self.request.query_params.get('categories', None)
+        rating = self.request.query_params.get('rating', None)
+        
+        queryset = Restaurant.objects.all()
+        
+        restaurant_category_rating_filter = RestaurantCategoryRating.objects.all()
+        
+        if categories:
+            category_list = categories.split(',')
+            restaurant_category_rating_filter = restaurant_category_rating_filter.filter(category__in = category_list)
+
+        if rating:
+            restaurant_category_rating_filter = restaurant_category_rating_filter.filter(rating__gte = rating)
+
+        return queryset.filter(restaurant_category_ratings__in = restaurant_category_rating_filter)
+
     
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
