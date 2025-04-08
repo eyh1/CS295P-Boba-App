ecf80d3dc84a009e622efd06b894f156836dd464
eyh1 <eryhuang@ucsc.edu>
2025-04-08 16:06:30 -0700

    stuff

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
+import sys
 import os
-
 from django.core.asgi import get_asgi_application
+
+# Add the top-level project dir to the path (the one with manage.py)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 settings_module = 'backend.backend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backend.backend.settings'
 
--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -7,9 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+import sys
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+# Add the top-level project dir to the path (the one with manage.py)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 
 settings_module = 'backend.backend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backend.backend.settings'
 
