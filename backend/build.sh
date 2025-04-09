30f6a9148053507c98deb78dedf967261d51f707
eyh1 <eryhuang@ucsc.edu>
2025-04-08 17:16:04 -0700

    a

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python backend/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
-python backend/manage.py migrate
+python manage.py migrate
