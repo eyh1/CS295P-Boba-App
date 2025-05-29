ecf139d86fa4c6e9b69136aebb620dccbbc6799e
Michael Wang <michaelwang9000@gmail.com>
2025-05-28 18:33:48 -0700

    added auto login upon register, error messages for login/register, and extended token lifetime

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,17 @@
 import Form from "../components/Form"
 import TopBar from "../components/TopBar"
+import { useLocation } from "react-router-dom";
 
 function Register() {
+  const location = useLocation();
+  const from = location.state || { from: "/" };
     return <div>
         <TopBar/>
         <div style={{ backgroundColor: " hsl(160, 36%, 95%)"}}>
         <h1 style={{ textAlign: 'center', marginTop: '0px', fontSize: '3rem', color: '#333' }}>
           Create an Account
         </h1>
-        <Form route = "/api/user/register/" method = "register"/>
+        <Form route = "/api/user/register/" method = "register" from={from}/>
         </div>
     </div>
     
